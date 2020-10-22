012ea781f4392817dc48806e58d900532b48de9d
Aaron Rose <github@acdr.dev>
2020-10-22 13:58:15 +1300

    use cache error path instead of config path, change default cache location

--- src/accessory.ts
+++ src/accessory.ts
@@ -221,7 +221,7 @@ export class YamahaAVRAccessory {
         } catch(err) {
           reject(`
             Could not access cache.
-            Please check your Homebridge instance has permission to access "${this.platform.config.cacheDirectory || '../.node-persist'}"
+            Please check your Homebridge instance has permission to access "${err.path}"
             or set a different cache directory using the "cacheDirectory" config property.
           `);
         }
@@ -311,7 +311,7 @@ export class YamahaAVRAccessory {
             reject(`
               Could not write to cache.
               Please check your Homebridge instance has permission to write to
-              "${this.platform.config.cacheDirectory || '../.node-persist'}"
+              "${err.path}"
               or set a different cache directory using the "cacheDirectory" config property.
             `);
           }
